299bb5e72c39e011ad0970da0d781a827e6f817e
anuj407 <anuj97953a@gmail.com>
2025-07-26 20:41:40 +0530

    feat: update DashboardProvider to include WelcomeHeader and adjust layout

--- app/(main)/provider.js
+++ app/(main)/provider.js
@@ -1,13 +1,15 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import React from 'react'
 import { AppSidebar } from './_components/AppSidebar'
+import WelcomeHeader from './dashboard/_components/WelcomeContainer'
 
 function DashboardProvider({children}) {
   return (
     <SidebarProvider>
       <AppSidebar/>
-        <div>
-          <SidebarTrigger/>
+        <div className='w-full'>
+          {/* <SidebarTrigger/> */}
+          <WelcomeHeader/>
           {children}
         </div>
     </SidebarProvider>
